2a631f00b90b9c8ecc679a8548f62ab434e44f71
xuanyimo <343557070@qq.com>
2015-11-16 11:31:37 +0800

    prepare to modified

--- circle/com.circle.dao/src/main/java/member/UserDao.java
+++ circle/com.circle.dao/src/main/java/member/UserDao.java
@@ -2,11 +2,7 @@ package member;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.stereotype.Repository;
-import utils.SqlUtil;
 
-import javax.annotation.Resources;
 import java.util.Map;
 
 /**
@@ -22,7 +18,6 @@ public class UserDao {
 
 	public SqlSession getSession(){
 		return sqlSessionFactory.openSession();
-
 	}
 
 	public void setSqlSessionFactoryBean(SqlSessionFactory sqlSessionFactory) {
